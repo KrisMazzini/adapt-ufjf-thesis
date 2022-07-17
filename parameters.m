ef63453a51a3490c3cee5f8c0ab79d5502757674
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-17 12:16:39 -0300

    adjust parameters

--- parameters.m
+++ parameters.m
@@ -1,6 +1,6 @@
 function [goal, maxSimTime, maxDistanceErr, maxAngleErr] = parameters()
 
-    goal = [-1.3; -1.6; adjustAngle(deg2rad(90))];
+    goal = [-5.3; 6; adjustAngle(deg2rad(90))];
     maxSimTime = 60; % s
     maxDistanceErr = 0.1;
     maxAngleErr = deg2rad(5);
